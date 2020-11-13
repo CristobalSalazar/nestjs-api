d16fe2f540fb0f306522d863d6498d4a391fd347
cristobal <chris.salza@gmail.com>
2020-11-13 00:52:55 -0500

    added create and exists methods

--- src/auth/email-verification.service.ts
+++ src/auth/email-verification.service.ts
@@ -7,6 +7,7 @@ import {
 } from './entities/email-verification.entity';
 import { Model } from 'mongoose';
 import { UsersService } from '../users/users.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class EmailVerificationService {
@@ -15,6 +16,22 @@ export class EmailVerificationService {
     @InjectModel(EmailVerification.name)
     private readonly emailVerificationModel: Model<EmailVerificationDocument>,
   ) {}
+
+  async createEmailVerification(uid: string, type: EmailVerificationType) {
+    return await this.emailVerificationModel.create({
+      type,
+      user: uid,
+      uuid: uuidv4(),
+    });
+  }
+
+  async existsForUser(uid: string, type: EmailVerificationType) {
+    const verification = await this.emailVerificationModel.findOne({
+      user: uid,
+      type,
+    });
+    return !!verification;
+  }
 
   async getEmailVerification(uuid: string, type: EmailVerificationType) {
     const emailVerification = await this.emailVerificationModel.findOne({
